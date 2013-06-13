003440590602259c84ec5589b47fab427ab37127
Jairo Alfaro <jalfaroe@gmail.com>
2013-06-13 02:40:53 -0600

    Adding Models and db

--- src/app/LiveAuction/models.py
+++ src/app/LiveAuction/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Auction(models.Model):
+	Id = models.AutoField(primary_key=True)
+	Title = models.CharField(max_length=100, blank=False)
+	Description = models.CharField(max_length=250, null=True, blank=True)
+	Hour = models.DateTimeField(auto_now=False)
+
+class Bid(models.Model):
+	Id = models.AutoField(primary_key=True)
+	Auction = models.ForeignKey(Auction)
+	User = models.ForeignKey(User)
+	Amount = models.DecimalField(max_digits=12, decimal_places=2)
+	Hour = models.DateTimeField(auto_now=True)
+
